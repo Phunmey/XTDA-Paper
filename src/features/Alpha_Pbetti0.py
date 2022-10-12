4e1d67d6e77b7fa97717f43a3b8cff74d433a5ad
Phunmey <funmilola@aims.edu.gh>
2022-10-12 00:42:10 -0500

    update PCA in features folder

--- src/features/Alpha_Pbetti0.py
+++ src/features/Alpha_Pbetti0.py
@@ -10,9 +10,11 @@ from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix
 from sklearn.model_selection import train_test_split, GridSearchCV
 from time import time
 
+
 random.seed(42)
 
-def read_csv(dataset):
+
+def reading_csv():
 
     df_edges = pd.read_csv(data_path + "/" + dataset + "/" + dataset + "_A.txt", header=None)  # import edge data
     df_edges.columns = ['from', 'to']
@@ -28,18 +30,18 @@ def read_csv(dataset):
     unique_graph_indicator = np.arange(min(graph_indicators),
                                        max(graph_indicators) + 1)  # list unique graph ids
 
-    X_train, X_test, y_train, y_test = train_test_split(unique_graph_indicator, graph_labels, test_size=0.2,
+    x_train, x_test, y_train, y_test = train_test_split(unique_graph_indicator, graph_labels, test_size=0.2,
                                                         random_state=42)
 
-    return X_train, X_test, y_train, y_test, graph_indicators, df_edges, graph_labels
+    return x_train, x_test, y_train, y_test, graph_indicators, df_edges, graph_labels
 
 
-def alpha_train(X_train, graph_indicators, df_edges, step_size):  # this is for the train data
+def alpha_train(x_train, graph_indicators, df_edges):  # this is for the train data
     start2 = time()
 
     train_betti = []
     h_0 = 0
-    for i in X_train:
+    for i in x_train:
         graph_id = i
         id_location = [index + 1 for index, element in enumerate(graph_indicators) if
                        element == graph_id]  # list the index of the graph_id locations
@@ -48,37 +50,34 @@ def alpha_train(X_train, graph_indicators, df_edges, step_size):  # this is for 
 
         if not create_traingraph.is_connected():
             graph_decompose = create_traingraph.decompose()
-           # print("I have" + str(len(graph_decompose)))
             mds_list = []
             for subg in graph_decompose:
                 create_subg = np.asarray(Graph.shortest_paths_dijkstra(subg))
                 norm_subg = create_subg / np.nanmax(create_subg)
-                mds = PCA(n_components=2).fit_transform(norm_subg)
+                if not np.linalg.det(norm_subg):
+                    conv_subg = np.linalg.pinv(norm_subg)  # use pseudoinverse if norm_subg is singular
+                    mds = PCA(n_components=2).fit_transform(conv_subg)
+                else:
+                    mds = PCA(n_components=2).fit_transform(norm_subg)
                 mds_list.append(mds)
-                #print("I  have done the first one" + str(subg))
             matrix_mds = (np.vstack(mds_list))
         else:
             create_dmatrix = np.asarray(Graph.shortest_paths_dijkstra(create_traingraph))
             norm_dmatrix = create_dmatrix / np.nanmax(create_dmatrix)
             matrix_mds = PCA(n_components=2).fit_transform(norm_dmatrix)
 
-        train_alpha_complex = gd.AlphaComplex(points=matrix_mds)
-        train_simplex_tree = train_alpha_complex.create_simplex_tree()
-        train_diagrams = np.asarray(train_simplex_tree.persistence(), dtype='object')
+        train_ac = gd.AlphaComplex(points=matrix_mds).create_simplex_tree()
+        train_dgm = train_ac.persistence()  # obtain persistence values
+        #    gd.plot_persistence_diagram(train_dgm)
+        #    plt.show()
 
-        # splitting the dimensions and obtain the maximum
-        train_persist_0 = train_diagrams[:, 1][np.where(train_diagrams[:, 0] == 0)]
-
-        if train_persist_0.size != 0:
-            max_1 = max(train_persist_0, key=lambda x: x[1] != np.inf)[1]
-            h_0 += 1
-        else:
-            max_1 = 0
+        #    select dimensions 0 and 1
+        train_dgm_0 = train_ac.persistence_intervals_in_dimension(0)
 
         train_betti_0 = []
-        for eps in np.linspace(0, max_1, step_size):
+        for eps in np.linspace(0, 1, step_size):
             b_0 = 0
-            for k in train_persist_0:
+            for k in train_dgm_0:
                 if k[0] <= eps and k[1] > eps:
                     b_0 = b_0 + 1
             train_betti_0.append(b_0)
@@ -93,12 +92,12 @@ def alpha_train(X_train, graph_indicators, df_edges, step_size):  # this is for 
     return train_data, train_time, h_0
 
 
-def alpha_test(X_test, graph_indicators, df_edges, step_size, train_time, h_0):  # this is for the train test
+def alpha_test(x_test, graph_indicators, df_edges, train_time, h_0):  # this is for the train test
     start3 = time()
 
     test_betti = []
     h_1 = 0
-    for j in X_test:
+    for j in x_test:
         graph_id = j
         id_location = [index + 1 for index, element in enumerate(graph_indicators) if
                        element == graph_id]  # list the index of the graph_id locations
@@ -110,8 +109,12 @@ def alpha_test(X_test, graph_indicators, df_edges, step_size, train_time, h_0): 
             mds_list = []
             for subg in graph_decompose:
                 create_subg = np.asarray(Graph.shortest_paths_dijkstra(subg))
-                norm_subg_ = create_subg / np.nanmax(create_subg)
-                mds = PCA(n_components=2).fit_transform(norm_subg_)
+                norm_subg = create_subg / np.nanmax(create_subg)
+                if not np.linalg.det(norm_subg):
+                    conv_subg = np.linalg.pinv(norm_subg)  # use pseudoinverse if norm_subg is singular
+                    mds = PCA(n_components=2).fit_transform(conv_subg)
+                else:
+                    mds = PCA(n_components=2).fit_transform(norm_subg)
                 mds_list.append(mds)
             matrix_mds = (np.vstack(mds_list))
         else:
@@ -119,24 +122,18 @@ def alpha_test(X_test, graph_indicators, df_edges, step_size, train_time, h_0): 
             norm_dmatrix = create_dmatrix / np.nanmax(create_dmatrix)
             matrix_mds = PCA(n_components=2).fit_transform(norm_dmatrix)
 
-        test_alpha_complex = gd.AlphaComplex(points=matrix_mds)  # initialize alpha complex
-        test_simplex_tree = test_alpha_complex.create_simplex_tree()  # creating a simplex tree
-        test_diagrams = np.asarray(test_simplex_tree.persistence(),
-                                   dtype='object') # run AlphaComplex filtration on the normalized distance matrix
+        test_ac = gd.AlphaComplex(points=matrix_mds).create_simplex_tree()
+        test_dgm = test_ac.persistence()  # obtain persistence values
+        #    gd.plot_persistence_diagram(train_dgm)
+        #    plt.show()
 
-        # splitting the dimensions and obtain the maximum
-        test_persist_0 = test_diagrams[:, 1][np.where(test_diagrams[:, 0] == 0)]
-
-        if test_persist_0.size != 0:
-            max_1 = max(test_persist_0, key=lambda x: x[1] != np.inf)[1]
-            h_1 += 1
-        else:
-            max_1 = 0
+        #    select dimensions 0 and 1
+        test_dgm_0 = test_ac.persistence_intervals_in_dimension(0)
 
         test_betti_0 = []
-        for eps in np.linspace(0, max_1, step_size):
+        for eps in np.linspace(0, 1, step_size):
             b_0 = 0
-            for k in test_persist_0:
+            for k in test_dgm_0:
                 if k[0] <= eps and k[1] > eps:
                     b_0 = b_0 + 1
             test_betti_0.append(b_0)
@@ -158,18 +155,20 @@ def tuning_hyperparameter():
     n_estimators = [int(a) for a in np.linspace(start=200, stop=500, num=5)]
     max_depth = [int(b) for b in np.linspace(start=2, stop=10, num=6)]
     num_cv = 10
+    bootstrap = [True, False]
     gridlength = len(n_estimators) * len(max_depth) * num_cv
     print(str(gridlength) + " RFs will be created in the grid search.")
-    Param_Grid = dict(n_estimators=n_estimators, max_depth=max_depth)
+    param_grid = dict(n_estimators=n_estimators, max_depth=max_depth, bootstrap=bootstrap)
 
-    return Param_Grid, num_cv
+    return param_grid, num_cv
 
 
-def random_forest(dataset, Param_Grid, train_data, test_data, y_train, y_test, alpha_time, num_cv, total_betti):
+def random_forest(param_grid, train_data, test_data, y_train, y_test, alpha_time, num_cv, total_betti):
     print(dataset + " training started at", datetime.now().strftime("%H:%M:%S"))
     start5 = time()
+
     rfc = RandomForestClassifier()
-    grid = GridSearchCV(estimator=rfc, param_grid=Param_Grid, cv=num_cv, n_jobs=10)
+    grid = GridSearchCV(estimator=rfc, param_grid=param_grid, cv=num_cv, n_jobs=10)
     grid.fit(train_data, y_train)
     param_choose = grid.best_params_
     if len(set(y_test)) > 2:  # multiclass case
@@ -195,22 +194,22 @@ def random_forest(dataset, Param_Grid, train_data, test_data, y_train, y_test, a
     flat_conf_mat = (str(conf_mat.flatten(order='C')))[
                     1:-1]  # flatten confusion matrix into a single row while removing the [ ]
     file.write(dataset + "\t" + str(alpha_time) + "\t" + str(training_time) +
-               "\t" + str(accuracy) + "\t" + str(auc) +  "\t" + str(total_betti) + "\t" + str(flat_conf_mat) + "\n")
+               "\t" + str(accuracy) + "\t" + str(auc) + "\t" + str(total_betti) + "\t" + str(flat_conf_mat) + "\n")
 
     file.flush()
 
 
 def main():
-    X_train, X_test, y_train, y_test, graph_indicators, df_edges, graph_labels = read_csv(dataset)
-    train_data, train_time, h_0 = alpha_train(X_train, graph_indicators, df_edges, step_size)
-    test_data, alpha_time, total_betti = alpha_test(X_test, graph_indicators, df_edges, step_size, train_time, h_0)
-    Param_Grid, num_cv = tuning_hyperparameter()
-    random_forest(dataset, Param_Grid, train_data, test_data, y_train, y_test, alpha_time, num_cv, total_betti)
+    x_train, x_test, y_train, y_test, graph_indicators, df_edges, graph_labels = reading_csv()
+    train_data, train_time, h_0 = alpha_train(x_train, graph_indicators, df_edges)
+    test_data, alpha_time, total_betti = alpha_test(x_test, graph_indicators, df_edges, train_time, h_0)
+    param_grid, num_cv = tuning_hyperparameter()
+    random_forest(param_grid, train_data, test_data, y_train, y_test, alpha_time, num_cv, total_betti)
 
 
 if __name__ == '__main__':
-    data_path = "/home/taiwo/projects/def-cakcora/taiwo/data"	#dataset path on computer
-    data_list = ('ENZYMES', 'BZR', 'MUTAG', 'PROTEINS', 'DHFR', 'NCI1', 'COX2')
+    data_path = "/home/taiwo/projects/def-cakcora/taiwo/data"  # dataset path on computer
+    data_list = ('ENZYMES', 'BZR', 'MUTAG', 'PROTEINS', 'DHFR', 'NCI1', 'COX2', 'REDDIT-MULTI-5K', 'REDDIT-MULTI-12K')
     outputFile = "/home/taiwo/projects/def-cakcora/taiwo/results3/" + 'Alpha_Pbetti0.csv'
     file = open(outputFile, 'w')
     for dataset in data_list:
@@ -220,5 +219,3 @@ if __name__ == '__main__':
     file.close()
 
 #
-
-
