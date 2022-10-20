9407925e67e9a143c04c71f977c47c284b24fe42
phunmey <funmilola@aims.edu.gh>
2022-10-20 15:18:36 -0500

    updating shapely code

--- src/shapely.py
+++ src/shapely.py
@@ -13,31 +13,35 @@ from sklearn.model_selection import cross_val_score, train_test_split
 random.seed(42)
 
 def data_csv(datapath):
-    read_data = pd.read_csv(datapath, sep=",")
-    read_data = read_data.drop(["Index", "graphlabel", "motif1", "motif2"], axis=1)
+    merged_df = pd.read_csv(datapath, sep=",")
+    read_data = merged_df.fillna(0)
 
-    X = read_data[read_data.columns[1:]]
-    X = X.fillna(0)  # Features
+#    split the motifs column into 4 different columns and drop columns
+    read_data[["motif1", "motif2", "motif3", "motif4"]] = read_data["motifs"].str.split(",", expand=True)
+    read_data = read_data.drop(["graph_label", "motifs", "motif1", "motif2"], axis=1)  # drop the index column from the dataframe
+
+    x = read_data[read_data.columns[1:]]
+#    X = X.fillna(0)  # Features
     y = read_data['dataset']  # Labels
 
-    return X, y
+    return x, y
 
-def RF_model(X, y):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    clf = RandomForestClassifier(n_estimators=300).fit(X_train, y_train)
-    y_pred = clf.predict(X_test)
+def rf_model(x, y):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+    clf = RandomForestClassifier(n_estimators=300).fit(x_train, y_train)
+    y_pred = clf.predict(x_test)
 
-    # model evaluation
-    accuracy = metrics.accuracy_score(y_test, y_pred) #obtain the accuracy score
-    accuracies = cross_val_score(estimator=clf, X=X_train, y=y_train, cv=10)
+#    model evaluation
+    accuracy = metrics.accuracy_score(y_test, y_pred)  # obtain the accuracy score
+    accuracies = cross_val_score(estimator=clf, X=x_train, y=y_train, cv=10)
     cross_validation = accuracies.mean()
-    roc_score = roc_auc_score(y_test, clf.predict_proba(X_test), multi_class='ovr') #obtain roc_auc_score
+    roc_score = roc_auc_score(y_test, clf.predict_proba(x_test), multi_class='ovr')  # obtain roc_auc_score
 #   plot_confusion_matrix(clf, X_test, y_test, display_labels=features, cmap=plt.cm.Blues, xticks_rotation='vertical' )
 #   plt.show()
-    return X_train, X_test, y_test, clf, accuracy, cross_validation, roc_score
+    return x_train, x_test, y_test, clf, accuracy, cross_validation, roc_score
 
-def vanilla_FI(X, X_test, y_test, clf):
-    # feature importance built-in the RandomForest algorithm (Mean Decrease in Impurity)
+def vanilla_fi(X, X_test, y_test, clf):
+#    feature importance built-in the RandomForest algorithm (Mean Decrease in Impurity)
     features = X.columns.values
     feature_importance = clf.feature_importances_
     std = np.std([tree.feature_importances_ for tree in clf.estimators_], axis=0)
@@ -45,10 +49,10 @@ def vanilla_FI(X, X_test, y_test, clf):
 
     fig, ax = plt.subplots()
     forest_importances.plot.bar(yerr=std, ax=ax)
-    ax.set_title("Feature importances using MDI")
-    ax.set_ylabel("Mean decrease in impurity")
+    ax.set_title("feature importances using mdi")
+    ax.set_ylabel("mean decrease in impurity")
     fig.tight_layout()
-    plt.savefig('C:Code/other_figures/FI_MDI2.png')
+    plt.savefig('C:Code/other_figures/fi_mdi2.png')
     #
     # #feature importance computed with permutation method
     perm_importance = permutation_importance(clf, X_test, y_test, n_repeats=10, random_state=42, n_jobs=2)
@@ -56,30 +60,30 @@ def vanilla_FI(X, X_test, y_test, clf):
 
     fig, ax = plt.subplots()
     perm_importances.plot.bar(yerr=perm_importance.importances_std, ax=ax)
-    ax.set_title("Feature importances using permutation on full model")
-    ax.set_ylabel("Mean accuracy decrease")
+    ax.set_title("feature importances using permutation on full model")
+    ax.set_ylabel("mean accuracy decrease")
     fig.tight_layout()
-    plt.savefig('C:Code/other_figures/FI_permutation2.png')
+    plt.savefig('C:Code/other_figures/fi_permutation2.png')
 
     return features
 
-def shap_vals(clf, X_train, features):
+def shap_vals(clf, x_train, features):
     # feature importance computed with SHAP_values (Global Interpretability) (bar plot)
     plt.clf()
     explainer = shap.TreeExplainer(clf)
-    shap_values = explainer.shap_values(X_train[:5])
+    shap_values = explainer.shap_values(x_train[:5])
     # print(explainer.expected_value) #the values obtained here serve as the base_values
     class_names = ["ENZYMES", "BZR", "COX2", "DHFR", "MUTAG", "NCI1", "PROTEINS", "REDDIT-MULTI-5K", "REDDIT-MULTI-12K"]
-    shap.summary_plot(shap_values, X_train, feature_names=features, class_names=class_names, show=False,
+    shap.summary_plot(shap_values, x_train, feature_names=features, class_names=class_names, show=False,
                       plot_size=(16, 10))
     plt.savefig('C:Code/other_figures/shap_dataset.png')
 # shap.summary_plot(shap_values[i (for i= 0, 1 ..., n)], X_test, feature_names=features, show=False) #for dot plot. This can be generated for a single class(or observation) at a time
 
     return shap_values, class_names
 
-def shap_feature_ranking(X_train, shap_values, features, class_names):
+def shap_feature_ranking(x_train, shap_values, features, class_names):
     c_idxs = []
-    idx = [c_idxs.append(X_train.columns.get_loc(column)) for column in features] # Get column locations for desired columns in given dataframe
+    idx = [c_idxs.append(x_train.columns.get_loc(column)) for column in features] # Get column locations for desired columns in given dataframe
     if isinstance(shap_values, list):  # If shap values is a list of arrays (i.e., several classes)
         means = [np.abs(shap_values[class_][:, c_idxs]).mean(axis=0) for class_ in
                  range(len(shap_values))]  # Compute mean shap values per class
@@ -115,11 +119,11 @@ def shap_feature_ranking(X_train, shap_values, features, class_names):
 #     return mean_std
 
 def main():
-    X, y = data_csv(datapath)
-    X_train, X_test, y_test, clf, accuracy, cross_validation, roc_score = RF_model(X, y)
-    features = vanilla_FI(X, X_test, y_test, clf)
-    shap_values, class_names = shap_vals(clf, X_train, features)
-    df_ranking = shap_feature_ranking(X_train, shap_values, features, class_names)
+    x, y = data_csv(datapath)
+    x_train, x_test, y_test, clf, accuracy, cross_validation, roc_score = rf_model(x, y)
+    features = vanilla_fi(x, x_test, y_test, clf)
+    shap_values, class_names = shap_vals(clf, x_train, features)
+    df_ranking = shap_feature_ranking(x_train, shap_values, features, class_names)
     #mean_std = avg_std(X_train, y, shap_values)
 
 if __name__ == '__main__':
