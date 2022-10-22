2e2d69be9686c73c1a189582fb55794a922f2824
Phunmey <funmilola@aims.edu.gh>
2022-10-21 22:44:25 -0500

    update mapper code

--- src/mapper.py
+++ src/mapper.py
@@ -1,4 +1,6 @@
-import webbrowser
+import os
+os.environ["OMP_NUM_THREADS"] = '1'  # this line is to prevent the warning I get wrt KMeans
+# import webbrowser
 import kmapper as km
 import pandas as pd
 import sklearn
@@ -6,18 +8,19 @@ from sklearn import manifold
 from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import MinMaxScaler
 
-
 # read data
-statistics = "/Users/taiwom1/XTDA-Paper/results/Obtain_gstatistics.csv"
+statistics = "C:/XTDA-Paper/results/Obtain_gstatistics.csv"
 reading_csv = pd.read_csv(statistics, sep=',')
 
 # split the motifs column into 4 different columns and drop columns
 reading_csv[["motif1", "motif2", "motif3", "motif4"]] = reading_csv["motifs"].str.split(",", expand=True)
-reading_csv = reading_csv.drop(["clustering_coeff", "graph_diameter", "motifs", "motif1", "motif2", "cliques", "components"], axis=1) #drop the index column from the dataframe
+reading_csv = reading_csv.drop(
+    ["clustering_coeff", "graph_diameter", "motifs", "motif1", "motif2", "cliques", "components"],
+    axis=1)  # drop the index column from the dataframe
 merged_df = reading_csv.fillna(0)
 
 # group data based on the smallest data length which is mutag
-df = merged_df.groupby(['dataset']).apply(lambda grp: grp.sample(n=187)) #mutag has 188 graphs
+df = merged_df.groupby(['dataset']).apply(lambda grp: grp.sample(n=187))  # mutag has 188 graphs
 
 y = df[['dataset']]  # select dataset column as the label for mapper purpose
 M = df[df.columns[1:]].apply(pd.to_numeric)  # convert object columns to numeric
@@ -25,31 +28,31 @@ M = M.drop(["graph_label"], axis=1)  # drop graph label column
 
 # mapper process
 Xfilt = M
-cls = len(pd.unique(merged_df.iloc[:,0]))  # return length of unique elements in the first column
+cls = len(pd.unique(merged_df.iloc[:, 0]))  # return length of unique elements in the first column
 mapper = km.KeplerMapper()  # initialize mapper
 scaler = MinMaxScaler(feature_range=(0, 1))  # scale fetaures
 print(list(M.columns))
 Xfilt = scaler.fit_transform(Xfilt)  # fit and transfrom features with scaler
-lens = mapper.fit_transform(Xfilt, projection=sklearn.manifold.TSNE(verbose=1)) #dimensionality reduction of Xfilt
-print("mapper started with "+str(len(pd.DataFrame(Xfilt).index))+" data points,"+str(cls)+" clusters")
+lens = mapper.fit_transform(Xfilt, projection=sklearn.manifold.TSNE(verbose=1))  # dimensionality reduction of Xfilt
+print("mapper started with " + str(len(pd.DataFrame(Xfilt).index)) + " data points," + str(cls) + " clusters")
 
 graph = mapper.map(
     lens,
     Xfilt,
     clusterer=sklearn.cluster.KMeans(n_clusters=cls, random_state=1618033),
     cover=km.Cover(n_cubes=10, perc_overlap=0.6)
-) # Create dictionary called 'graph' with nodes, edges and meta-information
+)  # Create dictionary called 'graph' with nodes, edges and meta-information
 print("mapper ended")
-print(str(len(y))+" "+str(len(Xfilt)))
+print(str(len(y)) + " " + str(len(Xfilt)))
 
 df['dataset_graphlabel'] = df['dataset'] + "-" + df['graph_label'].astype(str)
-df = df.drop(["dataset","graph_label"], axis=1)
+df = df.drop(["dataset", "graph_label"], axis=1)
 y_visual = df.dataset_graphlabel
 
 html = mapper.visualize(
     graph,
-    path_html="/Users/taiwom1/XTDA-Paper/mapper_plot/mapper.html",
+    path_html="C:/XTDA-Paper/mapper_plot/mapper.html",
     title="mapper data",
-    custom_tooltips=y_visual) # Visualize the graph
+    custom_tooltips=y_visual)  # Visualize the graph
 
-webbrowser.open('mapper.html')
+# webbrowser.open('mapper.html')
