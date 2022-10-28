a482a68f0c3367336c77139efc21f642487b5a10
Phunmey <funmilola@aims.edu.gh>
2022-10-28 10:39:50 -0500

    updated diameter boxplot code

--- src/max_distance.py
+++ src/max_distance.py
@@ -8,7 +8,7 @@ random.seed(42)
 read_df = pd.read_csv("C:/XTDA-Paper/results/Obtain_gstatistics.csv")
 read_df = read_df.fillna(0)
 
-boxplot = read_df.boxplot(column='graph_diameter', by='dataset', rot=45, grid=False, figsize=(16, 12), fontsize=12)
+boxplot = read_df.boxplot(column='graph_diameter', by='dataset', rot=45, grid=False, figsize=(16, 12), fontsize=18)
 
 #    remove all automated labels and titles
 boxplot.get_figure().gca().set_xlabel("")
@@ -16,8 +16,10 @@ boxplot.get_figure().suptitle('')
 boxplot.get_figure().gca().set_title("")
 
 #    set labels
-boxplot.set_xlabel('dataset', fontdict={'fontsize': 14})
-boxplot.set_ylabel('graph_diameter', fontdict={'fontsize': 15})
+boxplot.set_xlabel('dataset', fontdict={'fontsize': 18})
+boxplot.set_ylabel('graph diameter', fontdict={'fontsize': 18})
+plt.tight_layout()
+#plt.show()
 plt.savefig("C:/XTDA-Paper/vectorization_plot/diameter_boxplot.png")
 
 
